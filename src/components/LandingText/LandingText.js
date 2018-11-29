bc3c50b58afa75503cae45b6d8491d7d9b812ae3
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2018-11-29 17:01:26 -0500

    Create remote repo

---
+++ src/components/LandingText/LandingText.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './style.scss';
+
+const LandingText = () => {
+  return (
+    <div className="landing-text">
+      <div className="landing-text__header">
+        F-210
+      </div>
+      <div className="landing-text__quote">
+        "THE FASTEST DRONE ON THE PLANET"
+      </div>
+      <div className="landing-text__source">
+        -The Wirecutter
+      </div>
+    </div>
+  );
+};
+
+export default LandingText;
