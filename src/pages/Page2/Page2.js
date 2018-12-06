5dea90559ef31f50c5efae19bb0c5817866e5757
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2018-12-06 16:51:18 -0500

    Complete layout for Page2 on mobile

--- src/pages/Page2/Page2.js
+++ src/pages/Page2/Page2.js
@@ -40,7 +40,37 @@ class Page2 extends Component {
         style={{ backgroundColor: '#fff' }}
         ref={(elem) => this.page = elem}
       >
-      <Drone page='page2'  />
+
+      <div className="page-2__images-container">
+        <Drone page={'page2'}  />
+
+        <div className="page-2__image-selectors">
+          <ul className="selectors">
+            <li className="selectors__1 active"></li>
+            <li className="selectors__2"></li>
+            <li className="selectors__3"></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="page-2__text-container">
+        <div className="page-2__text-header">
+          <div className="page-2__text-title">
+            Craft and design.
+          </div>
+          <div className="page-2__title-underline" />
+        </div>
+
+        <div className="page-2__text">
+          Breakthrough design makes F-210 essentially improved.
+          With a new bow body design and carbon fiber material, it has
+          superior ruggedness and anti-drop ability. Through improved
+          small details, it will definitely give you more satisfaction.
+          Axis equidistant design ensures the gravity center of the aircraft
+          in the middle, which makes the flight more stable and turning agiler.
+          Motor protection cover makes flying.
+        </div>
+      </div>
 
       </div>
     );
