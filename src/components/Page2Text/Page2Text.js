d14dceadbd79458f87775baccb21e8f6fe5e8035
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2018-12-09 12:54:38 -0500

    Complete Page2 layout for desktop

--- src/components/Page2Text/Page2Text.js
+++ src/components/Page2Text/Page2Text.js
@@ -5,21 +5,28 @@ import './style.scss';
 const Page2Text = () => {
   return (
     <div className="page-2__text-container">
-      <div className="page-2__text-header">
-        <div className="page-2__text-title">
-          Craft and design.
-        </div>
-        <div className="page-2__title-underline" />
+      <div className="page-2__text-sidebar">
+        <p className="page-2__text-sidebar-content">
+          F-210 PERFORMANCE RACING DRONE
+        </p>
       </div>
+      <div className="page-2__text-body">
+        <div className="page-2__text-header">
+          <div className="page-2__text-title">
+            Craft and design.
+          </div>
+          <div className="page-2__title-underline" />
+        </div>
 
-      <div className="page-2__text">
-        Breakthrough design makes F-210 essentially improved.
-        With a new bow body design and carbon fiber material, it has
-        superior ruggedness and anti-drop ability. Through improved
-        small details, it will definitely give you more satisfaction.
-        Axis equidistant design ensures the gravity center of the aircraft
-        in the middle, which makes the flight more stable and turning agiler.
-        Motor protection cover makes flying.
+        <div className="page-2__text">
+          Breakthrough design makes F-210 essentially improved.
+          With a new bow body design and carbon fiber material, it has
+          superior ruggedness and anti-drop ability. Through improved
+          small details, it will definitely give you more satisfaction.
+          Axis equidistant design ensures the gravity center of the aircraft
+          in the middle, which makes the flight more stable and turning agiler.
+          Motor protection cover makes flying safer.
+        </div>
       </div>
     </div>
   );
--- src/pages/Page2/Page2.js
+++ src/pages/Page2/Page2.js
@@ -6,36 +6,30 @@ import Page2Text from '../../components/Page2Text';
 
 import './style.scss';
 
-// this.props.scrollHelper(this.page)
-
 class Page2 extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      droneWidth: null,
-      droneHeight: null,
+      displayed_img: [
+        {
+          id: 0,
+          title: 'Drone',
+          selected: true,
+          key: 'displayed_img'
+        },
+        {
+          id: 1,
+          title: 'Propeller',
+          selected: false,
+          key: 'displayed_img'
+        },
+      ]
     };
-
-    this.handleResize = this.handleResize.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-    this.setState({
-      droneWidth: window.innerWidth > 1026 ? '429px' : window.innerWidth > 757 ? '440px' : '322px',
-      droneHeight: window.innerWidth > 1026 ? '333px' : window.innerWidth > 757 ? '340px' : '246px'
-    });
-  }
-
-  handleResize() {
-    this.setState({
-      droneWidth: window.innerWidth > 1026 ? '429px' : window.innerWidth > 757 ? '440px' : '322px',
-      droneHeight: window.innerWidth > 1026 ? '333px' : window.innerWidth > 757 ? '340px' : '246px',
-    });
   }
 
   render() {
-    const { droneWidth, droneHeight } = this.state;
+    const { displayed_img } = this.state;
     return (
       <div
         className="page-2"
